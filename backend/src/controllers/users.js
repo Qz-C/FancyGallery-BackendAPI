9a21897571091d47ab8808a6b68ef6531c536360
Ezequiel Fonseca <31161704+Qz-C@users.noreply.github.com>
2020-06-07 06:27:24 -0300

    token generate implemented

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,9 +1,17 @@
 const db = require('../db/connection');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json')
 
 db.connect()
     .then(res => console.log('Successful connected to database'))
     .catch(e => console.error(e.stack))
+
+function genarateToken( params = {} ){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 604800,
+    })
+}
 
 module.exports = {
     async create(req, res){
@@ -20,12 +28,39 @@ module.exports = {
                 return res.status(409).send({error: 'Email already used'});
             else{
                 const hashedPassword = await bcrypt.hash(password, 10);
-                await db.query( 'INSERT INTO users (name, email, password, created_at, updated_at) VALUES($1, $2, $3, $4, $5)',
+                const user = await db.query( 'INSERT INTO users (name, email, password, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *',
                             [ name, email, hashedPassword, new Date(), new Date() ] );
-                return res.sendStatus(201);
-            }     
+                
+                user.rows[0].password = undefined;
+                return res.status(201).send({
+                    user: user.rows[0],
+                    token: genarateToken({email: email}),
+                });
+        }
+
         }catch{
             return res.sendStatus(500);
         }
     },
+
+    async session( req, res ) {
+        const { email, password } = req.body;
+
+        const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+        
+        if(user.rowCount === 0)
+            return res.status(400).send({error: "User not found"});
+
+        const hashedPassword = user.rows[0].password;
+        user.rows[0].password = undefined;
+
+        if ( ! await bcrypt.compare(password, hashedPassword) ) 
+            return res.status(400).send({error: "Invalid password"});
+
+        return res.status(201).send({
+            user: user.rows[0], 
+            token: genarateToken({email: email})
+        });
+    },
+
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,6 @@ const usersController = require('./controllers/users');
 const route = express.Router();
 
 route.post('/register', usersController.create);
+route.post('/authenticate', usersController.session);
 
 module.exports = route;
